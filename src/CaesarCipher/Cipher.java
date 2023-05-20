13f514cd08d052ef237ea1aa04e6e69f822c94db
SerhiiKo <drizer84@gmail.com>
2023-05-20 08:53:34 +0300

    Initial commit

---
+++ src/CaesarCipher/Cipher.java
@@ -0,0 +1,12 @@
+package CaesarCipher;
+
+import Constants.Alphabets;
+
+public class Cipher {
+    private Alphabets alphabet;
+    private int key;
+    public Alphabets selectAlphabets(String example){
+        return null;
+    }
+
+}
---
+++ src/Constants/Alphabets.java
@@ -0,0 +1,6 @@
+package Constants;
+
+public class Alphabets {
+    public static char[] english;
+    public static char[] ukrainian;
+}
